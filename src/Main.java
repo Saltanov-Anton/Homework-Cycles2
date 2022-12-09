026b3332a1d1663052bc52a48c2ffae219564d92
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-09 22:17:06 +0900

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int totalEnd = 2_459_000;
+        int totalNow = 0;
+        int contribution = 15000;
+        int month = 0;
+        while (totalNow <= totalEnd) {
+            totalNow += contribution;
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + totalNow + " рублей");
+        }
+    }
+
 }
