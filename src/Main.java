d949c9e7f5ba1ebcd7d4e3211372455c86478019
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-11 11:54:31 +0900

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -27,6 +28,17 @@ public class Main {
         System.out.println();
         for (int i = 0; i < number; number--) {
             System.out.print(number + " ");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int population = 12_000_000;
+        for (int i = 1; i <= 10; i++) {
+            int fertility = population / 1000 * 17;
+            int mortality = population / 1000 * 8;
+            population += fertility - mortality;
+            System.out.println("Год " + i + ", численность населения составляет " + population);
         }
     }
 
